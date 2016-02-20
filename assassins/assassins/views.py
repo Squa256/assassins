97940ed6ddd7d50feb47a932be096be5b223b1f0
Luke Hurst <hurstlj@umich.edu>
2016-02-20 04:08:03 -0500

    Modify login view to be a post endpoint

--- assassins/assassins/views.py
+++ assassins/assassins/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import views as auth_views
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
 	pass
 
+@require_POST
 def login(request, **kwargs):
 	if request.user.is_authenticated():
 		return redirect('index')
