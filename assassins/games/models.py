d362f1a913fbb34e460533c571f72f2b8df0aee5
Luke Hurst <hurstlj@umich.edu>
2016-02-20 01:24:52 -0500

    Add models for User and Game

--- assassins/games/models.py
+++ assassins/games/models.py
@@ -3,3 +3,53 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+	max_num_players = models.PositiveIntegerField()
+	start_date = models.DateTimeField(
+		blank = False,
+	)
+	round_length = models.DurationField(
+		blank = False,
+	)
+
+	PLAYER_APPROVAL_CHOICES = (
+		(0, 'Admin'),
+		(1, 'All'),
+	)
+	approval_preference = models.PositiveIntegerField(
+		default = 0,
+		choices = PLAYER_APPROVAL_CHOICES,
+	)
+
+class Membership(models.Model):
+	game = models.ForeignKey(
+		Game,
+		blank = False,
+		on_delete = models.CASCADE,
+	)
+	user = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		blank = False,
+		on_delete = models.CASCADE,
+	)
+	current_target = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		null = True,
+		on_delete = models.SET_NULL,
+	)
+	is_alive = models.BooleanField(
+		default = True,
+	)
+
+	MEMBERSHIP_CHOICES = (
+		(0, 'Confirmed'),
+		(1, 'Invited'),
+		(2, 'Requested'),
+	)
+	membership_status = models.PositiveIntegerField(
+		blank = False,
+		choices = MEMBERSHIP_CHOICES,
+	)
+	is_admin = models.BooleanField(
+		default = False,
+	)
--- assassins/users/models.py
+++ assassins/users/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+# Default user gives us some fields for free
+#  username, first_name, last_name, email, is_staff, is_active, date_joined
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+class User(AbstractBaseUser):
+	profile_picture_url = models.CharField(
+		max_length=200,
+		blank=False,
+	)
+	major = models.CharField(max_length=50)
