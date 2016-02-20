08f483689d512069647c2395272ec9a20a884c6e
Cooper Young <coopery@umich.edu>
2016-02-20 00:24:04 -0500

    Added users & games apps, created url structure

--- assassins/assassins/urls.py
+++ assassins/assassins/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+	url(r'^$', views.index),
+	url(r'^login$', views.login),
+	url(r'^user/', include('users')),
+	url(r'^game/', include('games')),
     url(r'^admin/', admin.site.urls),
 ]
---
+++ assassins/games/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+	url(r'^(?P<game_id>[0-9]+)$', views.homepage),
+	url(r'^create$', views.create),
+]
