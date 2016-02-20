8d4c5a2739110e6ecb1569ee25d565fc60d204f0
Cooper Young <coopery@umich.edu>
2016-02-20 06:25:26 -0500

    Added user homepage

--- assassins/assassins/views.py
+++ assassins/assassins/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import views as auth_views
 
+from games.models import Membership
+
 # Create your views here.
 def index(request):
+	if request.user.is_authenticated():
+		targets = Membership.objects.filter(user=request.user)
+		return render(request, 'assassins/home.html',
+				{'user': request.user, 'targets': targets})
+
 	return render(request, 'assassins/index.html')
 
 def login(request, **kwargs):
