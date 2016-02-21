dc1a7eecf3902f96b1bc2ac521a0b055d52fcd89
Luke Hurst <hurstlj@umich.edu>
2016-02-20 20:22:19 -0500

    Add ability to report a kill and protest a kill

--- assassins/assassins/views.py
+++ assassins/assassins/views.py
@@ -6,25 +6,38 @@ from games.models import Membership
 from users.models import User
 
 # Create your views here.
+def claim_kill(request):
+	killed_username_form = KilledUsernameForm(request.POST)
+	if killed_username_form.is_valid():
+		killed_user = User.objects.get(username=killed_username_form.cleaned_data['username'])
+		member = Membership.objects.get(user=killed_user)
+		member.is_alive = False
+		member.save()
+	else:
+		#TODO: Handle invalid form
+		pass
+
+def confirm_or_deny_kill(request):
+	if request.POST.get('confirm'):
+		Membership.objects.get(user=request.user).delete()
+	elif request.POST.get('deny'):
+		#Set 24 hour timer until user is removed
+		pass
+
 def index(request):
 	if request.user.is_authenticated():
-		targets = Membership.objects.filter(user=request.user)
+		memberships = Membership.objects.filter(user=request.user)
 
 		if request.method == 'POST':
-			killed_username_form = KilledUsernameForm(request.POST)
-			if killed_username_form.is_valid():
-				print("Killing user", killed_username_form.cleaned_data['username'])
-				user = User.objects.get(username=killed_username_form.cleaned_data['username'])
-				member = Membership.objects.get(user=user)
-				member.is_alive = False
-				member.save()
-		else:
-			killed_username_form = KilledUsernameForm()
+			if request.POST.get('username'):
+				claim_kill(request)
+			else:
+				confirm_or_deny_kill(request)
 
-		return render(request, 'assassins/home.html', {
-			'targets': targets,
+		return render(request, 'assassins/home.html',{
+			'memberships': memberships,
+			'killed_username_form': KilledUsernameForm(),
 			'user': request.user,
-			'killed_username_form': killed_username_form,
 		})
 
 	return render(request, 'assassins/index.html')
